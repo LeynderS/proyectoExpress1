10ce49773be43ba749daad34ea7ec65ce1eb6f66
Leynder <leynder.elian@gmail.com>
2025-07-01 19:12:56 -0500

    Agregar vista fotos y tabla con lectura de servicio

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/photos', async function(req, res, next) {
+  const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json'
+  // const config = {
+  //   proxy: {
+  //     host: 'localhost',
+  //     port: 4444
+  //   }
+  // }
+  const response = await axios.get(URL)
+  res.render('fotos', { title: 'Fotos', fotos: response.data });
 });
 
 module.exports = router;
